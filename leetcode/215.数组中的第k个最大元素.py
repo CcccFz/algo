424f14633160c6e592699175efd87709aa4b2143
CcccFz <ccccfz@163.com>
2023-01-14 16:59:04 +0800

    nlogn sort

--- leetcode/215.数组中的第k个最大元素.py
+++ leetcode/215.数组中的第k个最大元素.py
@@ -4,7 +4,41 @@
 # [215] 数组中的第K个最大元素
 #
 
+from typing import List
 # @lc code=start
+import random
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        self.k = k-1
+        self.quick_sort(nums, 0, len(nums)-1)
+        return nums[self.k]
+
+    def quick_sort(self, nums: List[int], start, end: int) -> int:
+        if start < end:
+            mid = self.partition(nums, start, end)
+            if mid == self.k:
+                return
+            elif mid > self.k:
+                return self.quick_sort(nums, start, mid-1)
+            elif mid < self.k:
+                return self.quick_sort(nums, mid+1, end)
+            
+    def partition(self, nums: List[int], start, end: int) -> int:
+        pivot_idx = random.randint(start, end)   
+        nums[end], nums[pivot_idx] = nums[pivot_idx], nums[end]
+
+        i = start
+        for j in range(start, end):
+            if nums[j] > nums[end]:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+        nums[i], nums[end] = nums[end], nums[i]
+        return i
+
+    # def findKthLargest(self, nums: List[int], k: int) -> int:
+    #     nums.sort()
+    #     return nums[-k]
 # @lc code=end
+s = Solution()
+print(s.findKthLargest([3,2,1,5,6,4], 2))
+print(s.findKthLargest([3,2,3,1,2,4,5,5,6], 4))
--- leetcode/239.滑动窗口最大值.py
+++ leetcode/239.滑动窗口最大值.py
@@ -3,8 +3,24 @@
 #
 # [239] 滑动窗口最大值
 #
+from typing import List
 
 # @lc code=start
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        size = len(nums)
+        if size <= 1 or k == 0:
+            return nums
+        ret, queue = [], []
+        for i in range(size):
+            if queue and queue[0] == i - k:
+                queue.pop(0)
+            while queue and nums[queue[-1]] < nums[i]:
+                queue.pop()
+            queue.append(i)
+            if i >= k - 1:
+                ret.append(nums[queue[0]])
+        return ret
 # @lc code=end
+s = Solution()
+print(s.maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
