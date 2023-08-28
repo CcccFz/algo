2cd12c1957fc28d69e8e819c8d8359ae13cc8d00
CcccFz <ccccfz@163.com>
2023-08-28 17:59:10 +0800

    update

--- beauty/05_sort/01_O_n2_sort.py
+++ beauty/05_sort/01_O_n2_sort.py
@@ -3,24 +3,24 @@ from typing import List
 def bubble_sort(nums: List[int]):
     size = len(nums)
     if size <= 1:
-        return nums
+        return
     for i in range(size-1):
-        is_flag = False
-        for j in range(size-i-1):
+        flag = False
+        for j in range(size-1-i):
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
-                is_flag = True
-        if not is_flag:
+                flag = True
+        if not flag:
             break
 
 def insert_sort(nums: List[int]):
     size = len(nums)
     if size <= 1:
-        return nums
+        return
     for i in range(1, size):
         j = i - 1
         num = nums[i]
-        while j >= 0 and nums[j] > num:
+        while j >= 0 and num < nums[j]:
             nums[j+1] = nums[j]
             j -= 1
         nums[j+1] = num
@@ -28,7 +28,7 @@ def insert_sort(nums: List[int]):
 def select_sort(nums: List[int]):
     size = len(nums)
     if size <= 1:
-        return nums
+        return
     for i in range(size):
         min = i
         for j in range(i+1, size):
@@ -39,9 +39,11 @@ def select_sort(nums: List[int]):
 nums = [3,2,1,5,4,6,7]
 bubble_sort(nums)
 print(nums)
+
 nums = [3,2,1,5,4,6,7]
 insert_sort(nums)
 print(nums)
+
 nums = [3,2,1,5,4,6,7]
 select_sort(nums)
 print(nums)
--- beauty/05_sort/02_O_nlogn_sort.py
+++ beauty/05_sort/02_O_nlogn_sort.py
@@ -19,10 +19,10 @@ def merge_sort(nums: List[int]):
             else:
                 tmp.append(nums[j])
                 j += 1
-        start, end = (i, mid) if i <= mid else (j, high)
+        start, end = i, mid if i <= mid else j, high
         tmp.extend(nums[start:end+1])
         nums[low:high+1] = tmp
-    
+
     merge_sort_idx(nums, 0, len(nums)-1)
 
 
@@ -39,27 +39,10 @@ def quick_sort(nums: List[int]):
 
         i = low
         for j in range(low, high):
-            if nums[j] < nums[high]:
-                nums[j], nums[i] = nums[i], nums[j]
-                i += 1
-        nums[i], nums[high] = nums[high], nums[i]
-        return i
-    
-    def partition2(nums, low, high):
-        idx = random.randint(low, high)
-        nums[idx], nums[high] = nums[high], nums[idx]
-        pivot = nums[high]
+            if nums[j] 
 
-        i, j = low, high
-        while i < j:
-            while i < j and nums[i] <= pivot:
-                i += 1
-            nums[j] = nums[i]
-            while i < j and nums[j] > pivot:
-                j -= 1
-            nums[i] = nums[j]
-        nums[i] = pivot
-        return i
+    def partition2(nums, low, high):
+        pass
 
     quick_sort_idx(nums, 0, len(nums)-1)
 
