f8dcda46800587a5c79dcc726527a4e0860f0026
CcccFz <ccccfz@163.com>
2023-11-14 23:06:32 +0800

    update

--- 13_thought/05_dp/02_double11.py
+++ 13_thought/05_dp/02_double11.py
@@ -1,26 +1,32 @@
-def double11(items, least):
+def double11(prices, least):
     states = [False] * (2*least+1)
-    states[0], states[items[0]] = True, True
-    for i in range(1, len(items)):
-        for j in range(2*least-items[i], -1, -1):
+    states[0], states[prices[0]] = True, True
+    for i in range(1, len(prices)):
+        for j in range(2*least-prices[i], -1, -1):
             if states[j]:
-                states[j+items[i]] = True
-    
-    price = -1
-    for j in range(least, 2*least+1):
-        if states[j]:
-            price = j
+                states[j+prices[i]] = True
+
+    price = 0
+    for i in range(least, 2*least+1):
+        if states[i]:
+            price = i
             break
-    picks = []
+
     _price = price
-    for i in range(len(items)-1, -1, -1):
-        if _price > 0 and states[_price-items[i]]:
-            picks.insert(0, items[i])
-            _price -= items[i]
+    picks = []
+    # TODO: 该方法是错误的，有问题
+    for i in range(len(prices)-1, -1, -1):
+        if _price == 0:
+            break
+        if _price >= prices[i] and states[_price-prices[i]]:
+            _price -= prices[i]
+            picks.insert(0, prices[i])
 
     return price, picks
 
 
-if __name__ == '__main__':
-    print(double11([10,20,30,20,70,60,40,30,40,10,20], 200))
-    print(double11([33,73,13,53,13,13,23,43,83,73,83], 200))
+# (200, [60, 40, 30, 40, 10, 20])
+print(double11([10, 20, 30, 20, 70, 60, 40, 30, 40, 10, 20], 200))
+
+# (202, [23, 73, 83])
+print(double11([33, 73, 13, 53, 13, 13, 23, 43, 83, 73, 83], 200))
--- 13_thought/05_dp/03_yh_triangle.py
+++ 13_thought/05_dp/03_yh_triangle.py
@@ -1,34 +1,22 @@
 def yh_triangle(nums):
     n = len(nums)
-    state = [[0]*n for _ in range(n)]
-    state[0][0] = nums[0][0]
-
-    for i in range(1, n):
-        for j in range(i+1):
-            if j == 0:
-                state[i][j] = nums[i][j] + state[i-1][j]
-            elif j == i:
-                state[i][j] = nums[i][j] + state[i-1][j-1]
-            else:
-                state[i][j] = nums[i][j] + min(state[i-1][j-1], state[i-1][j])
-    return min(state[-1])
-
-def yh_triangle2(nums):
-    n = len(nums)
-    state = [0] * n
-    state[0] = nums[0][0]
-
+    states = [0] * n
+    states[0] = nums[0][0]
     for i in range(1, n):
         for j in range(i, -1, -1):
             if j == i:
-                state[j] = nums[i][j] + state[j-1]
+                states[j] = nums[i][j] + states[j-1]
             elif j == 0:
-                state[j] = nums[i][j] + state[j]
+                states[j] = nums[i][j] + states[j]
             else:
-                state[j] = nums[i][j] + min(state[j-1], state[j])
-    return min(state)
+                states[j] = nums[i][j] + min(states[j-1], states[j])
+    return min(states)
 
-if __name__ == '__main__':
-    nums = [[3], [2, 6], [5, 4, 2], [6, 0, 3, 2]]
-    print(yh_triangle(nums))   # 9
-    print(yh_triangle2(nums))  # 9
+
+nums = [
+    [3],
+    [2, 6],
+    [5, 4, 2],
+    [6, 0, 3, 2],
+]
+print(yh_triangle(nums))   # 9
