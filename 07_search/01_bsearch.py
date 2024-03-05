47d602d41b87be6a0ceee75db10b8582c87406f1
CcccFz <ccccfz@163.com>
2024-03-05 10:52:21 +0800

    update

--- 07_search/01_bsearch.py
+++ 07_search/01_bsearch.py
@@ -1,30 +1,28 @@
-from typing import List
-
-def bsearch(nums: List[int], target: int) -> int:
-    low, high = 0, len(nums)-1
-    while low <= high:
-        mid = low + ((high-low)>>1)
+def bsearch(nums, target):
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + ((r-l)>>1)
         if nums[mid] < target:
-            low = mid + 1
+            l = mid + 1
         elif nums[mid] > target:
-            high = mid - 1
+            r = mid - 1
         else:
             return mid
     return -1
 
-def bsearch_recursion(nums: List[int], target: int) -> int:
-    def bsearch_idx(nums, low, high):
-        if low > high:
+def bsearch_recursion(nums, target):
+    def bsearch_idx(l, r):
+        if l > r:
             return -1
-        mid = low + ((high-low)>>1)
+        mid = l + ((r-l)>>1)
         if nums[mid] < target:
-            return bsearch_idx(nums, mid+1, high)
+            return bsearch_idx(mid+1, r)
         elif nums[mid] > target:
-            return bsearch_idx(nums, low, mid-1)
+            return bsearch_idx(l, mid-1)
         else:
             return mid
-
-    return bsearch_idx(nums, 0, len(nums)-1)
+    
+    return bsearch_idx(0, len(nums)-1)
 
 print(bsearch([-1,0,3,5,9,12], target = 9))
 print(bsearch_recursion([-1,0,3,5,9,12], target = 9))
--- 07_search/02_bsearch_variant.py
+++ 07_search/02_bsearch_variant.py
@@ -1,70 +1,68 @@
 def bsearch_first(nums, target):
-    low, high = 0, len(nums)-1
-    while low <= high:
-        mid = low + ((high-low)>>1)
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + ((r-l)>>1)
         if nums[mid] < target:
-            low = mid + 1
+            l = mid + 1
         elif nums[mid] > target:
-            high = mid - 1
+            r = mid - 1
         else:
             if mid == 0 or nums[mid-1] != target:
                 return mid
-            high = mid - 1
+            r = mid - 1
     return -1
 
 def bsearch_last(nums, target):
-    low, high = 0, len(nums)-1
-    last = high
-    while low <= high:
-        mid = low + ((high-low)>>1)
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + ((r-l)>>1)
         if nums[mid] < target:
-            low = mid + 1
+            l = mid + 1
         elif nums[mid] > target:
-            high = mid - 1
+            r = mid - 1
         else:
-            if mid == last or nums[mid+1] != target:
+            if mid == len(nums)-1 or nums[mid+1] != target:
                 return mid
-            low = mid + 1
+            l = mid + 1
     return -1
 
 def bsearch_first_not_less(nums, target):
-    low, high = 0, len(nums)-1
-    while low <= high:
-        mid = low + ((high-low)>>1)
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + ((r-l)>>1)
         if nums[mid] < target:
-            low = mid + 1
+            l = mid + 1
         else:
             if mid == 0 or nums[mid-1] < target:
                 return mid
-            high = mid - 1
+            r = mid - 1
     return -1
 
 def bsearch_last_not_greater(nums, target):
-    low, high = 0, len(nums)-1
-    last = high
-    while low <= high:
-        mid = low + ((high-low)>>1)
+    l, r = 0, len(nums)-1
+    while l <= r:
+        mid = l + ((r-l)>>1)
         if nums[mid] > target:
-            high = mid - 1
+            r = mid - 1
         else:
-            if mid == last or nums[mid+1] > target:
+            if mid == len(nums)-1 or nums[mid+1] > target:
                 return mid
-            low = mid + 1
+            l = mid + 1
     return -1
 
 def my_sqrt(x):
-    low, high = 0, x
-    while low <= high:
-        mid = low + (high-low)/2
+    l, r = 0, x
+    while l <= r:
+        mid = l + (r-l)/2
         ret = mid * mid
         if ret > x:
-            high = mid
+            r = mid
         else:
-            if x - ret <= 0.000001:
+            if x-ret < 0.000001:
                 return round(mid, 6)
             else:
-                low = mid
-    return -1            
+                l = mid
+    return -1
 
 a = [1, 1, 2, 3, 4, 6, 7, 7, 7, 7, 10, 22]
 print(bsearch_first(a, 0) == -1)
