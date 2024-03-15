9b0b6edc337033a886db67dd6c8e37ec78013a04
CcccFz <ccccfz@163.com>
2024-03-15 17:58:47 +0800

    update

--- core/05_sort/02_O_nlogn_sort.py
+++ core/05_sort/02_O_nlogn_sort.py
@@ -64,31 +64,6 @@ def quick_sort(nums: List[int]):
 
     quick_sort_idx(0, len(nums)-1)
 
-
-def heap_sort(nums: List[int]):
-    def heapify(nums, high, i):
-        while True:
-            max_i = i
-            if i*2+1 <= high and nums[i*2+1] > nums[max_i]:
-                max_i = i*2+1
-            if i*2+2 <= high and nums[i*2+2] > nums[max_i]:
-                max_i = i*2+2
-            if max_i == i:
-                break
-            nums[max_i], nums[i] = nums[i], nums[max_i]
-            i = max_i
-    
-    def build_heap(nums, high):
-        for i in range((high-1)//2, -1, -1):
-            heapify(nums, high, i)
-    
-    high = len(nums)-1
-    build_heap(nums, high)
-    while high:
-        nums[high], nums[0] = nums[0], nums[high]
-        high -= 1
-        heapify(nums, high, 0)
-
 if __name__ == '__main__':
     nums = [3,2,1,5,4,6,7]
     merge_sort(nums)
@@ -96,8 +71,4 @@ if __name__ == '__main__':
     
     nums = [3,2,1,5,4,6,7]
     quick_sort(nums)
-    print(nums)
-
-    nums = [3,2,1,5,4,6,7]
-    heap_sort(nums)
     print(nums)
--- core/07_binary_tree/02_heap.py
+++ core/07_binary_tree/02_heap.py
@@ -64,7 +64,34 @@ class Heap:
         print(self.vals[1:self.len+1])
         self.draw()
         return ''
-            
+
+
+def heap_sort(nums):
+    def heapify(nums, high, i):
+        while True:
+            max_i = i
+            if i*2+1 <= high and nums[i*2+1] > nums[max_i]:
+                max_i = i*2+1
+            if i*2+2 <= high and nums[i*2+2] > nums[max_i]:
+                max_i = i*2+2
+            if max_i == i:
+                break
+            nums[max_i], nums[i] = nums[i], nums[max_i]
+            i = max_i
+    
+    def build_heap(nums, high):
+        for i in range((high-1)//2, -1, -1):
+            heapify(nums, high, i)
+    
+    high = len(nums)-1
+    build_heap(nums, high)
+    while high:
+        nums[high], nums[0] = nums[0], nums[high]
+        high -= 1
+        heapify(nums, high, 0)
+
+
+
 heap = Heap(100, [7,17,5,13,15,1,8,6,16,21,2,9,33])
 print(heap)
 
@@ -82,3 +109,8 @@ print(heap)
 
 heap.remove_top()
 print(heap)
+
+
+nums = [3,2,1,5,4,6,7]
+heap_sort(nums)
+print(nums)
