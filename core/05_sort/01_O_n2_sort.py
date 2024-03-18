569c9f39912ef51ecdde9bca04abedf418f48634
CcccFz <ccccfz@163.com>
2024-03-18 21:31:14 +0800

    update

--- core/05_sort/01_O_n2_sort.py
+++ core/05_sort/01_O_n2_sort.py
@@ -1,23 +1,17 @@
-from typing import List
-
-def bubble_sort(nums: List[int]):
-    size = len(nums)
-    if size <= 1:
-        return
-    for i in range(size-1):
+def bubble_sort(nums):
+    n = len(nums)
+    for i in range(n-1):
         flag = False
-        for j in range(size-1-i):
+        for j in range(n-1-i):
             if nums[j] > nums[j+1]:
                 nums[j], nums[j+1] = nums[j+1], nums[j]
                 flag = True
         if not flag:
             break
 
-def insert_sort(nums: List[int]):
-    size = len(nums)
-    if size <= 1:
-        return
-    for i in range(1, size):
+def insert_sort(nums):
+    n = len(nums)
+    for i in range(1, n):
         j = i - 1
         num = nums[i]
         while j >= 0 and num < nums[j]:
@@ -25,13 +19,11 @@ def insert_sort(nums: List[int]):
             j -= 1
         nums[j+1] = num
 
-def select_sort(nums: List[int]):
-    size = len(nums)
-    if size <= 1:
-        return
-    for i in range(size):
+def select_sort(nums):
+    n = len(nums)
+    for i in range(n):
         min = i
-        for j in range(i+1, size):
+        for j in range(i+1, n):
             if nums[j] < nums[min]:
                 min = j
         nums[i], nums[min] = nums[min], nums[i]
