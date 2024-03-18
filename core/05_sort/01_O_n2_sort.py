e29d79a5e56e68cb645182725921c00da21fb6da
CcccFz <ccccfz@163.com>
2024-03-18 21:55:08 +0800

    update

--- core/05_sort/01_O_n2_sort.py
+++ core/05_sort/01_O_n2_sort.py
@@ -21,7 +21,7 @@ def insert_sort(nums):
 
 def select_sort(nums):
     n = len(nums)
-    for i in range(n):
+    for i in range(n-1):
         min = i
         for j in range(i+1, n):
             if nums[j] < nums[min]:
