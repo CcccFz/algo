c5405a4261491f4425f00682e1e4138873516d21
CcccFz <ccccfz@163.com>
2024-03-21 17:59:20 +0800

    update

--- core/08_graph/01_graph.py
+++ core/08_graph/01_graph.py
@@ -10,19 +10,16 @@ class Graph:
     
     def bfs(self, _from, to):
         if _from == to:
-            return [_from]
-        visited = [False] * len(self)
+            return [to]
+        prev, visited, queue = [None]*len(self), [False]*len(self), [_from]
         visited[_from] = True
-        prev = [None] * len(self)
-        queue = [_from]
         while queue:
             i = queue.pop(0)
             for j in self.vertex[i]:
                 if visited[j]:
                     continue
-                visited[j] = True
-                prev[j] = i
-                if j == to:                
+                visited[j], prev[j] = True, i
+                if j == to:
                     ret = [j]
                     while prev[j] is not None:
                         ret.insert(0, prev[j])
@@ -33,47 +30,40 @@ class Graph:
     def find_vertex_at_degree(self, _from, degree):
         if degree == 0:
             return [_from]
-        visited = [False] * len(self)
+        visited, queue, ret = [False]*len(self), [_from], []
         visited[_from] = True
-        queue = [_from]
         while queue:
+            degree -= 1
             for _ in range(len(queue)):
                 i = queue.pop(0)
                 for j in self.vertex[i]:
                     if visited[j]:
                         continue
                     visited[j] = True
+                    ret.append(j)
                     queue.append(j)
-            degree -= 1
             if degree == 0:
-                return queue
+                break
+        return ret
 
     def dfs(self, _from, to):
         def _dfs(i):
             nonlocal found
-            if found:
+            if found or visited[i]:
                 return
             visited[i] = True
-            if i == to:
-                ret.insert(0, i)
-                while prev[i] is not None:
-                    ret.insert(0, prev[i])
-                    i = prev[i]
-                return
             for j in self.vertex[i]:
-                if visited[j]:
-                    continue
                 prev[j] = i
+                if j == to:
+                    ret.append(to)
+                    while prev[j] is not None:
+                        ret.insert(0, prev[j])
+                        j = prev[j]
+                    found = True
+                    return
                 _dfs(j)
 
-        if _from == to:
-            return [_from]
-        
-        ret = []
-        found = False
-        visited = [False] * len(self)
-        prev = [None] * len(self)
-
+        visited, prev, found, ret = [False]*len(self), [None]*len(self), False, []
         _dfs(_from)
         return ret
 
@@ -99,7 +89,7 @@ if __name__ == '__main__':
     graph.add_edge(6, 7)
     print(graph)
     print(graph.bfs(0, 7))    # [0, 1, 2, 5, 7]
-    print(graph.find_vertex_at_degree(0, 3)) # 3:5,6 4:7
+    print(graph.find_vertex_at_degree(0, 3)) # 4度多个7
     print(graph.dfs(0, 7))  # [0, 1, 2, 5, 4, 6, 7]
 
 
@@ -113,5 +103,5 @@ if __name__ == '__main__':
 # 7 [5, 6]
 
 # [0, 1, 2, 5, 7]
-# [5, 6]
+# [1, 3, 2, 4, 5, 6]
 # [0, 1, 2, 5, 4, 6, 7]
